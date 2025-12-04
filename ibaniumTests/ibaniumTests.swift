17d73c15b59e3241cdd129eed1a9d3e7e7a243aa
ozgungungor <ozgun.gungor>
2025-12-04 19:49:42 +0300

    Initial Commit

---
+++ ibaniumTests/ibaniumTests.swift
@@ -0,0 +1,17 @@
+//
+//  ibaniumTests.swift
+//  ibaniumTests
+//
+//  Created by özgün güngör on 4.12.2025.
+//
+
+import Testing
+@testable import ibanium
+
+struct ibaniumTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
